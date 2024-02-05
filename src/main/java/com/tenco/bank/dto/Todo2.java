b7258e8de533cbb342fbbf94164c361bcd2f2a1c
junyeoke <junyeoke@kakao.com>
2024-02-05 13:33:34 +0900

    이미지 업로드 수정

---
+++ src/main/java/com/tenco/bank/dto/Todo2.java
@@ -0,0 +1,14 @@
+package com.tenco.bank.dto;
+
+import lombok.Data;
+
+
+@Data
+public class Todo2 {
+	
+	private Integer id;
+	private String title;
+	private String body;
+	private Integer userId;
+
+}
